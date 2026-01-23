69d58e8285cde167f649060ccbccf3b2bf05234e
xaatim <xayari229@gmail.com>
2026-01-23 20:21:46 +0800

    changes

---
+++ src/generic_robot_driver/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages # Added find_packages
+from glob import glob
+
+package_name = 'generic_robot_driver'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    # FIX: Use find_packages() instead of manually listing paths with slashes
+    packages=find_packages(exclude=['test']), 
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch') , glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'config') , glob(os.path.join('config', '*'))),
+        (os.path.join('share', package_name, 'meshes') , glob(os.path.join('meshes', '*.stl'))),
+        (os.path.join('share', package_name, 'urdf') , glob(os.path.join('urdf', '*.*'))),
+        (os.path.join('share', package_name, 'urdf/realsense') , glob(os.path.join('urdf/realsense', '*.*'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world'))),
+        (os.path.join('share', package_name, 'maps'), glob(os.path.join('maps', '*.yaml'))),
+    
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'generic_robot_driver = generic_robot_driver.robot_base_driver:main'
+        ],
+    },
+)
